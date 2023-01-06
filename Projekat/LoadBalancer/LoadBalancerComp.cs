56811729282fc883f4195b5f8051edfa3fca0f21
dusandulo <dusandulo@DUSANDULO3EBF>
2023-01-06 19:56:57 +0100

    Dodat buffer za LoadBalancer komponentu

--- Projekat/LoadBalancer/LoadBalancerComp.cs
+++ Projekat/LoadBalancer/LoadBalancerComp.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Common;
+using Common;
 
 namespace LoadBalancer
 {
-    public class Class1
+    public class LoadBalancerComp
     {
+        public Buffer buffer { get; set; }
+
+        public LoadBalancerComp()
+        {
+            buffer = new Buffer();
+        }
     }
 }
