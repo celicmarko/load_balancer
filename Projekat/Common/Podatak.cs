f8e7d86c58023a76b7cabeb015771151335ea542
celicmarko <marko.celic13@gmail.com>
2023-01-06 17:03:18 +0100

    Napravljena struktura podataka koja treba da se salje

---
+++ Projekat/Common/Podatak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Podatak
+    {
+        public int Id { get; set; }
+        public double Vrednost { get; set; }
+
+        public Podatak()
+        {
+        }
+
+        public Podatak(int id, double vrednost)
+        {
+            Id = id;
+            Vrednost = vrednost;
+        }
+    }
+}
