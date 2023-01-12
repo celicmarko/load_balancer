9326097938e3c5fa317c95c87051de44e76cec22
celicmarko <marko.celic13@gmail.com>
2023-01-12 19:05:00 +0100

    Azurirana klasa Podatak
    Dodati parametri iz tabela i uradjena provera

--- Projekat/Common/Podatak.cs
+++ Projekat/Common/Podatak.cs
@@ -9,25 +9,33 @@ namespace Common
     public class Podatak
     {
         public int Id { get; set; }
-        public double Vrednost { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Ulica { get; set; }
+        public int Broj { get; set; }
+        public string Grad { get; set; }
+        public int Post { get; set; }
 
         public Podatak()
         {
         }
 
-        public Podatak(int id = 0, double vrednost = 0)
+        public Podatak(int id, string ime, string prezime, string ulica, int broj, string grad, int post)
         {
-            if (id > 0)
+            if (id < 0 || ime == "" || prezime == "" || ulica == "" || broj < 0 || grad == "" || post < 0)
             {
-                Id = id;
+                throw new ArgumentException("Polje ne sme biti prazno");
             }
             else
             {
-                throw new ArgumentException("Id mora biti veci od 0");
+                Id = id;
+                Ime = ime;
+                Prezime = prezime;
+                Ulica = ulica;
+                Broj = broj;
+                Grad = grad;
+                Post = post;
             }
-
-            Vrednost = vrednost;
-        }
-
+        }   
     }
 }
