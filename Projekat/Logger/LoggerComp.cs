90918c7044f960b6df96024de82b064d4c9189e3
celicmarko <marko.celic13@gmail.com>
2023-01-18 14:13:36 +0100

    Dodata komponenta Logger

---
+++ Projekat/Logger/LoggerComp.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class LoggerComp
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(LoggerService)))
+            {
+                serviceHost.AddServiceEndpoint(typeof(ISlanjePoruke), new NetTcpBinding(), new Uri("net.tcp://localhost:5500/ISlanjePoruke"));
+                serviceHost.Open();
+
+                while (true)
+                {
+                    if (LoggerService.primljenaPoruka != "")
+                    {
+                        Console.WriteLine(LoggerService.primljenaPoruka);
+                        LoggerService.primljenaPoruka = "";
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Projekat/Logger/LoggerService.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class LoggerService : ISlanjePoruke
+    {
+        public static string primljenaPoruka = "";
+
+        public void SlanjeMerenja(string merenje)
+        {
+            primljenaPoruka = merenje;    
+        }
+    }
+}
